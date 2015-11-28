4b751ed781158fedb092cb760e8acc22b9912946
Alice M <alicetyma@gmail.com>
2015-11-28 03:45:57 -0500

    Add function to draw scalable and gate

--- js/draw.js
+++ js/draw.js
@@ -1,12 +1,50 @@
 $(document).ready(function() {
     var ctx = $('#canvas').get(0).getContext('2d');
 
-    function draw() {
-        ctx.beginPath();
-        ctx.moveTo(0,0);
-        ctx.lineTo(500, 500);
-        ctx.stroke();
+    function draw_buttons() {
+      // uhh
     }
 
-    draw();
+    function draw_and_gate(width, height){
+      //to-do: figure out how to get the start coordinates
+      var start_x = 50;
+      var start_y = 50;
+      ctx.beginPath();
+
+      //draw the circle part of the and
+      ctx.arc(start_x, start_y, height/2, -0.5 * Math.PI, 0.5 * Math.PI);
+      console.log("CIRCLE PART")
+      ctx.stroke();
+
+      //draw the rectangle part of the and gate
+      ctx.moveTo(start_x, start_y + height/2)
+      ctx.lineTo(start_x - width/2, start_y + height/2);
+      ctx.lineTo(start_x - width/2, start_y - height/2);
+      ctx.lineTo(start_x, start_y - height/2);
+      ctx.stroke();
+    }
+
+    function draw_or_gate(width, height){
+
+    }
+
+    draw_and_gate(30, 30);
+
+    function render() {
+      // redraw everything!!!
+      // using the circuit global
+    }
+
+    function simulate() {
+        updateAgenda();
+        for (var i = 0; i < agenda.length; i++) {
+            render();
+            var tasks = agenda[i];
+            for (var j = 0; j < tasks.length; j++) {
+              tasks[j]();
+            }
+        }
+    }
+
+    draw_buttons();
 });
