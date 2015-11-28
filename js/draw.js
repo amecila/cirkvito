f0945b5229e5aa147b4ac3f7f75c199724476b69
Alice M <alicetyma@gmail.com>
2015-11-28 05:52:44 -0500

    #perfectionism to a fault????

--- js/draw.js
+++ js/draw.js
@@ -1,41 +1,74 @@
 $(document).ready(function() {
     var ctx = $('#canvas').get(0).getContext('2d');
+    var f = 30;
+
 
     function draw_buttons() {
-      // uhh
+      //draw 6 rectangular buttons
+      var start_x = 20 //20px of space between button and border
+    //  var start_y =
+    // var space = 20// spacing between rows: height = 30px and soace between buttons is 10px
+    //  for (i = 0; i < 5; i ++)
+    //    ctx.rect(start_x, start_y + space, 40, 100)
+
+    //  }
     }
 
-    function draw_and_gate(width, height){
-      //to-do: figure out how to get the start coordinates
-      var start_x = 50;
-      var start_y = 50;
+    function draw_and_gate(gate) {
+      var x0 = gate.pos[0];
+      var y0 = gate.pos[1];
+
       ctx.beginPath();
 
       //draw the circle part of the and gate
-      ctx.arc(start_x, start_y, height/2, -0.5 * Math.PI, 0.5 * Math.PI);
+      ctx.arc(x0 + f, y0 + f/2, f/2, -0.5 * Math.PI, 0.5 * Math.PI);
       ctx.stroke();
 
       //draw the rectangle part of the and gate
-      ctx.moveTo(start_x, start_y + height/2)
-      ctx.lineTo(start_x - width/2, start_y + height/2);
-      ctx.lineTo(start_x - width/2, start_y - height/2);
-      ctx.lineTo(start_x, start_y - height/2);
+      ctx.moveTo(x0 + f, y0 + f)
+      ctx.lineTo(x0, y0 + f);
+      ctx.lineTo(x0, y0);
+      ctx.lineTo(x0 + f, y0);
       ctx.stroke();
     }
 
-    function draw_or_gate(width, height){
-      var start_x = 40;
-      var start_y = 80;
+    function draw_or_gate(gate){
+      var x0 = gate.pos[0];
+      var y0 = gate.pos[1];
+
       ctx.beginPath();
-      ctx.moveTo(start_x, start_y);
-      ctx.quadraticCurveTo(start_x + 150, start_y + 20, start_x, start_y + 50);
-      //ctx.quadraticCurveTo(200, 70, 50, 100);
-      ctx.quadraticCurveTo(start_x + 50, start_y + 20, start_x, start_y);
+
+      //front part
+      ctx.moveTo(x0, y0);
+      ctx.quadraticCurveTo(x0 + 3 * f, y0 + 0.5 * f, x0, y0 + f);
+
+      //back part
+      ctx.quadraticCurveTo(x0 + 0.5*f, y0 + 0.5 * f, x0, y0);
       ctx.stroke();
     }
 
-    draw_and_gate(30, 30);
-    draw_or_gate(30, 30);
+    function draw_not_gate(gate){
+      var x0 = gate.pos[0];
+      var y0 = gate.pos[1];
+
+      ctx.beginPath();
+
+      //triangle part
+      ctx.moveTo(x0, y0);
+      ctx.lineTo(x0 + 1.3 * f, y0 + 0.5 * f);
+      ctx.lineTo(x0, y0 + f);
+      ctx.closePath();
+
+      //circle part
+      var r = 0.1 * f;
+      ctx.moveTo(x0 + 1.3 * f + 2 * r, y0 + 0.5 * f)
+      ctx.arc(x0 + 1.3 * f + r, y0 + 0.5 * f, r, 0, Math.PI*2);
+      ctx.stroke();
+    }
+
+    draw_and_gate({pos: [30, 30]});
+    draw_or_gate({pos: [30, 70]});
+    draw_not_gate({pos: [30, 110]});
 
     function render() {
       // redraw everything!!!
